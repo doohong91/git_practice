751c1314426a47382f0ec6354d1ca1fae9112c1f
doohong91 <doohong91@gmail.com>
2019-01-03 10:24:41 +0900

    updated at home

--- browser.py
+++ browser.py
@@ -1,19 +1,20 @@
 import webbrowser
 
 # webbrowser.open("https://www.daum.net")
-
-
-## 1. 사용자의 입력을 받아 검색하기
-# keyword = input('원하는 리그를 입력해 주세요: ')
-
-## 2. 여러 페이지를 한번에 열기
 url = 'https://sports.media.daum.net/sports/schedule/'
 
-league = ['epl','bundesliga','primera','seriea','ligue1','eredivisie',
-'uefacl','uefacup','eplcup','facup']
+## 1. 사용자의 입력을 받아 검색하기
 
-for l in league:
-    webbrowser.open(url + l)
+keyword = input('원하는 리그를 입력해 주세요: ')
+webbrowser.open(url + keyword)
+
+## 2. 여러 페이지를 한번에 열기
+
+# league = ['epl','bundesliga','primera','seriea','ligue1','eredivisie',
+# 'uefacl','uefacup','eplcup','facup']
+# for l in league:
+#     webbrowser.open(url + l)
+
 
 
 
